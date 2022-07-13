6eceae417ced3c6745051e9142470bc4ce5db6c4
gary <gary.white@lodgit.com>
2022-07-13 12:48:42 +0200

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function descendingOrder(n) {
+    //...
+}
+
+console.log(descendingOrder(0)); // 0
+console.log(descendingOrder(1)); // 1
+console.log(descendingOrder(111)); // 111
+console.log(descendingOrder(15)); // 51
+console.log(descendingOrder(1021)); // 2110
+console.log(descendingOrder(0123456789)); // 987654321
